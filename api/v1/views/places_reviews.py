88e65853ef33ec7230eaf949d202a7f1cc1e49e3
kel2ck <kelvinonumajuru@gmail.com>
2023-05-01 16:04:38 -0700

    handles all default RESTFul API actions

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,83 +1,67 @@
 #!/usr/bin/python3
-"""
-Contains the places_reviews view for the API
-"""
-from api.v1.views import app_view
-from flask import make_response, abort, jsonify, request
-from models import storage
+""" objects that handle all default RestFul API actions for Reviews """
 from models.review import Review
-from models.user import User
 from models.place import Place
-from flasgger.utils import swag_from
+from models.user import User
+from models import storage
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
 
 
-@app_views.route('/places/<string:place_id>/reviews',
-                 methods=['GET'], strict_slashes=False)
-@swag_from('documentation/reviews/get.yml', methods=['GET'])
-def get_all_reviews(place_id):
-    def get_all_reviews(place_id):
-    """ get reviews from a spcific place """
+@app_views.route('/places/<place_id>/reviews', methods=['GET'], strict_slashes=False)
+def get_reviews(place_id):
+    """
+    Retrieves the list of all Review objects of a Place
+    """
     place = storage.get(Place, place_id)
-    if place is None:
+    if not place:
         abort(404)
-    reviews = [obj.to_dict() for obj in place.reviews]
-
+    reviews = [review.to_dict() for review in place.reviews]
     return jsonify(reviews)
 
 
 @app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/reviews/get_review.yml', methods=['GET'])
 def get_review(review_id):
     """
-    Gets the review with the given id
+    Retrieves a Review object
     """
     review = storage.get(Review, review_id)
     if not review:
         abort(404)
-
     return jsonify(review.to_dict())
 
 
-@app_views.route('/reviews/<string:review_id>', methods=['DELETE'],
-                 strict_slashes=False)
-@swag_from('documentation/reviews/delete.yml', methods=['DELETE'])
-def del_review(review_id):
-    """ remove review by id"""
+@app_views.route('/reviews/<review_id>', methods=['DELETE'], strict_slashes=False)
+def delete_review(review_id):
+    """
+    Deletes a Review Object
+    """
     review = storage.get(Review, review_id)
-    if review is None:
+    if not review:
         abort(404)
-    review.delete()
+    storage.delete(review)
     storage.save()
-    return jsonify({}, 200)
+    return make_response(jsonify({}), 200)
 
 
-@app_views.route('/places/<place_id>/reviews', methods=['POST'],
-                 strict_slashes=False)
-@swag_from('documentation/reviews/post_reviews.yml', methods=['POST'])
-def create_obj_review(place_id):
+@app_views.route('/places/<place_id>/reviews', methods=['POST'], strict_slashes=False)
+def post_review(place_id):
     """
     Creates a Review
     """
     place = storage.get(Place, place_id)
-
     if not place:
         abort(404)
-
     if not request.get_json():
         abort(400, description="Not a JSON")
-
     if 'user_id' not in request.get_json():
         abort(400, description="Missing user_id")
-
     data = request.get_json()
     user = storage.get(User, data['user_id'])
-
     if not user:
         abort(404)
-
     if 'text' not in request.get_json():
         abort(400, description="Missing text")
-
     data['place_id'] = place_id
     instance = Review(**data)
     instance.save()
@@ -85,21 +69,16 @@ def create_obj_review(place_id):
 
 
 @app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
-@swag_from('documentation/reviews/put_reviews.yml', methods=['PUT'])
 def put_review(review_id):
     """
     Updates a Review
     """
     review = storage.get(Review, review_id)
-
     if not review:
         abort(404)
-
     if not request.get_json():
         abort(400, description="Not a JSON")
-
     ignore = ['id', 'user_id', 'place_id', 'created_at', 'updated_at']
-
     data = request.get_json()
     for key, value in data.items():
         if key not in ignore:
